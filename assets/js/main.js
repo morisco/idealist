c59a38022efb21bfb29a2b58ef8f3de42a733906
Mike Morisco <mike.morisco@jet.com>
2016-11-29 20:54:00 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,6 @@ $(document).ready(function(){
     $(window).on('scroll', adjustShare);
     $(window).on('resize', adjustSize);
 
-
-
     $('#action-panel .slides').on('init', function(e, slick, current, next){
         var width   = $('#slide-gif-1').width(),
             height  = $('#slide-gif-1').height(),
@@ -35,9 +33,7 @@ $(document).ready(function(){
             left: left + 'px'
         }).empty().append('<img src="' + $('#slide-gif-1').attr('src') + '" />')
 
-        setTimeout(function(){
-            $('.image-fixed').addClass('visible');
-        },250)
+        $('.image-fixed').addClass('visible');
     });
 
     var slick = $('#action-panel .slides').slick({
@@ -56,10 +52,11 @@ $(document).ready(function(){
         var gifSrc = $('#slide-gif-' + (next+1)).attr('src') + '?x' + date.getTime();
         // $('#slide-gif-' + (next+1)).css('opacity', '0');
         // $('#slide-gif-' + (next+1)).attr('src', gifSrc).css('opacity', 1);
-        $('.image-fixed').addClass('fade');
     });
 
     $('#action-panel .slides').on('afterChange', function(e, slick, index){
+        var date = new Date();
+
         var width   = $('#slide-gif-' + (index+1)).width(),
             height  = $('#slide-gif-' + (index+1)).height(),
             top     = $('#slide-gif-' + (index+1)).offset().top,
@@ -72,21 +69,27 @@ $(document).ready(function(){
             height: height + 'px',
             top: top + 'px',
             left: left + 'px'
-        }).empty().append('<img src="' + $('#slide-gif-' + (index+1)).attr('src') + '" />');
+        }).empty().append('<img src="' + $('#slide-gif-' + (index+1)).attr('src') + '?x' + date.getTime() +'" />');
 
-        setTimeout(function(){
-            $('.image-fixed').addClass('visible');
-        },250)
+        $('.image-fixed').addClass('visible');
     });
 
     function adjustSize() {
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(function() {
-            if(shareFixed){
-                $('#page-shares').removeClass('fixed').removeAttr('style');
-                shareOffset.left = $('#page-shares').offset().left;
-                $('#page-shares').css('left', shareOffset.left + 'px').addClass('fixed');    
-            }
+            var width   = $('#slide-gif-' + (index+1)).width(),
+                height  = $('#slide-gif-' + (index+1)).height(),
+                top     = $('#slide-gif-' + (index+1)).offset().top,
+                left    = $('#slide-gif-' + (index+1)).offset().left;
+
+            $('.image-fixed').removeClass('fade visible');
+            $('.image-fixed').css({
+                position: 'fixed',
+                width: width + 'px',
+                height: height + 'px',
+                top: top + 'px',
+                left: left + 'px'
+            })
         }, 250);
     }
 
